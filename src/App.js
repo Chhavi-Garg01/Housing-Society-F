a3257029f2b35c013b65436c31abd02002277954
Chhavi-0104 <chhavi@gkmit.co>
2022-11-02 17:51:39 +0530

    files Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
-import Header from './Components/navbar';
-import Login from './Containers/login';
+import Auth from './cmp/Auth';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './cmp/About';
+import Home from './cmp/Home';
+import Protected from './cmp/Protected';
+import Listing from './cmp/Listing';
+import Nav from './cmp/Nav';
+
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Login />
+      <Router>
+        <Nav />
+
+        <Routes>
+          <Route path="/about" element={<Protected cmp={About}/>}>
+          </Route>
+          <Route path="/home" element={<Protected cmp={Home}/>}>
+          </Route>
+          <Route path="/list" element={<Protected cmp={Listing}/>}>
+          </Route>
+          <Route path="/" element={<Auth />}>
+          </Route>
+        </Routes>
+     </Router>
     </div>
   );
 }
---
+++ src/cmp/Protected.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import {Navigate} from "react-router-dom"
+function Protected(props){
+    const Cmp=props.cmp;
+    var auth=JSON.parse(localStorage.getItem('auth'))
+    console.warn(auth)
+    return <div>{ auth ? <Cmp />:<Navigate to="/"></Navigate>}</div>
+}
+
+export default Protected;
