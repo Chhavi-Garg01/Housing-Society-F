41878b94adc7dc6c34855e5593031bbcd9662bb3
Chhavi-0104 <chhavi@gkmit.co>
2022-11-04 11:44:38 +0530

    Bookings Updated

--- my-app/src/components/Bookings.js
+++ my-app/src/components/Bookings.js
@@ -1,10 +1,39 @@
 import React , {Component} from 'react'
 
 class Bookings extends Component {
-    render(){
+    constructor(){
+        super()
+        this.state={
+            items:null
+        }
+    }
+    componentDidMount()
+    {
+        fetch('http://127.0.0.1:8000/bookings').then((result)=>{
+            result.json().then((data)=>{
+                this.setState({items:data})
+            })
+        })
+    }
+    render() {
         return (
             <div>
-                Bookings
+                <span> id<span className="tab"></span>Resource<span className="tab"></span> user_id Date <span className="tab"></span> Booked</span>
+                {
+                    this.state.items?
+                    this.state.items.map((item)=>
+                    <div>
+                        <table>
+                            <tr>
+                                <th>{item.id}</th>
+                                <td>{item.resource_name}</td>
+                                <td>{item.Date_Booked}</td>
+                                <td>{item.user_id}</td>
+                            </tr>
+                        </table>
+                    </div>
+                    ):null
+                }
             </div>
         )
     }
