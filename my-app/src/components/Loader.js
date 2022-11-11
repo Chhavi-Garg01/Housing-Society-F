20d6ee79916b71fea888734a2a5ba9ac224be20c
Chhavi-0104 <chhavi@gkmit.co>
2022-11-11 12:22:37 +0530

    Updated: All components to functional component and added Booking feature.

---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
+
+function Loader() {
+    let [loading, setLoading] = useState(true);
+    return (
+        <div style={{ marginTop: '150px' }}>
+            <div className="sweet-loading text-center">
+                <ClipLoader
+                    color="#000"
+                    loading={loading}
+                    css=''
+                    size={80}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
